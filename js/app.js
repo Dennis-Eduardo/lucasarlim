13db1b3abb26dca18623bdb3fa31e2727767474c
Lucas Araújo de Lima <70279700+lucasarlim@users.noreply.github.com>
2021-03-09 18:31:58 -0300

    add projects and contacts

--- js/app.js
+++ js/app.js
@@ -2,4 +2,9 @@ var hamburguer = document.querySelector(".hamburguer");
 
 hamburguer.addEventListener("click", function(){
     document.querySelector(".nav-bar").classList.toggle("show-menu");
+    document.querySelector(".close").classList.toggle("show-span");
 });
+
+
+
+
